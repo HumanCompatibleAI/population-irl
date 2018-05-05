9680c2f16c4961ac3655aabcbd8ac5fef10844fb
Adam Gleave <adam@gleave.me>
2018-05-04 17:59:12 -0700

    [experiments] config: fix reacher-env-comparisons

--- pirl/experiments/config.py
+++ pirl/experiments/config.py
@@ -352,7 +352,7 @@ EXPERIMENTS['billiards'] = {
 EXPERIMENTS['reacher-env-comparisons'] = {
     'environments': ['Reacher-v2', 'pirl/Reacher-baseline-seed0-v0',
                      'pirl/Reacher-variable-hidden-goal-seed0-v0',
-                     'pirl/Reacher-fixed-hidden-goal-seed{}-v0'],
+                     'pirl/Reacher-fixed-hidden-goal-seed0-v0'],
     'discount': 0.99,
     'expert': 'ppo_cts',
     'eval': ['ppo_cts'],
