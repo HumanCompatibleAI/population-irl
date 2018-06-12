6c03c36aa6c44ea849f8433f0f7dd99426492dfc
Adam Gleave <adam@gleave.me>
2018-06-12 12:41:24 -0700

    experiments: new Reacher experiment

--- pirl/config/config.py
+++ pirl/config/config.py
@@ -487,13 +487,13 @@ EXPERIMENTS['mountain-car-multigoals'] = {
     'irl': ['airl_so_short', 'airl_sa_short', 'airl_random_short'],
     'test_trajectories': [1, 2, 5, 100],
 }
-EXPERIMENTS['reacher-var'] = {
+EXPERIMENTS['reacher-multigoals'] = {
     'environments': ['pirl/Reacher-seed{}-{}-v0'.format(seed, noise)
                      for seed in range(0,3) for noise in [0.1, 0.5, 1.0]],
     # simple environment, small number of iterations sufficient to converge
     'expert': 'ppo_cts_200k',
-    'irl': ['airl_so_short'],
-    'eval': [],
+    'irl': ['airl_so_short', 'airl_sa_short', 'airl_random_short'],
+    'eval': ['ppo_cts_200k'],
     'test_trajectories': [1, 2, 5, 100],
 }
 
